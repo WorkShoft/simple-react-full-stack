d83171cf58cb09134a7a5ba67fca12590922d678
Mikel Losada <mikel.losada@gmail.com>
2018-06-11 12:52:46 +0200

    Display JSON in local server

--- src/client/App.js
+++ src/client/App.js
@@ -1,27 +1,50 @@
 import React, { Component } from 'react';
 import './app.css';
 
+
 export default class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { username: null };
+      this.state = { username: null, worldCup: null };
+
   }
 
   componentDidMount() {
-    fetch('/api/getUsername')
-      .then(res => res.json())
-      .then(user => this.setState({ username: user.username }));
+      fetch('/api/getUsername')
+          .then(res => res.json())
+          .then(user => this.setState({ username: user.username }));
+
+      fetch('/api/getWorldCup')
+          .then(res => res.json())
+          .then(worldCup => this.setState({worldCup: worldCup}));
   }
 
-  render() {
-    return (
-      <div>
-        {this.state.username ? (
-          <h1>Hello {this.state.username}</h1>
-        ) : (
+    render() {
+
+        return (
+
+                <div>
+          {this.state.worldCup ? (
+                  <div>{JSON.stringify(this.state.worldCup['stadiums'])}</div>
+            ) : (
           <h1>Loading.. please wait!</h1>
-        )}
-      </div>
+            )}
+
+            </div>
+
+      // <div>
+      //       {this.state.username ? (
+      //     <h1>Hello {this.state.username}</h1>
+      //       ) : (
+      //     <h1>Loading.. please wait!</h1>
+      //     )}
+
+
+      //   </div>
+
+        
+        
+
     );
-  }
+    }
 }
--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const os = require('os');
-
 const app = express();
 
+var worldCup = require('./worldCupData.json');
+
+    
 app.use(express.static('dist'));
 app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
+app.get('/api/getWorldCup', (req, res) => res.send(worldCup));
+
 app.listen(8080, () => console.log('Listening on port 8080!'));
